d0a3e1b9ba457bb7fe55b41c1f7b59899670a290
aki0207 <forever.look.aki@docomo.ne.jp>
2019-03-13 16:53:27 +0900

    Initial Commit

---
+++ OverSleptExcuse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OverSleptExcuse
+//
+//  Created by ききょ on 2019/03/13.
+//  Copyright © 2019 kikyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
